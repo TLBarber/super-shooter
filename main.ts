b5ee3022ce5da5f48e56e2a1d54feb55269d5be7
Thomas L Barber Jr <Thomas.l.barber.jr@gmail.com>
2021-02-03 19:06:51 -0500

    Added rapid/auto fire indicator, fixed fire before setup issue

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
 namespace SpriteKind {
     export const powerup = SpriteKind.create()
+    export const autofire_power = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     music.jumpUp.play()
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.autofire_power, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.smiles, 200)
+    music.magicWand.play()
+    autofire += 1
+    autofire_indicator = textsprite.create("x1000")
+    autofire_indicator.setIcon(img`
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+        6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+        6 9 6 6 9 9 c c 6 9 9 9 6 6 9 6 
+        6 6 6 9 9 9 9 c 6 6 9 9 9 6 6 6 
+        6 6 9 9 9 9 9 9 6 6 9 9 9 9 6 6 
+        6 9 9 9 6 6 6 6 9 6 9 9 c 9 9 6 
+        6 9 c 6 6 6 9 9 9 6 9 c c c 9 6 
+        6 9 c c c 9 6 9 9 9 6 6 6 c 9 6 
+        6 9 9 c 9 9 6 9 6 6 6 6 9 9 9 6 
+        6 6 9 9 9 9 6 6 9 9 9 9 9 9 6 6 
+        6 6 6 9 9 9 6 6 c 9 9 9 9 6 6 6 
+        6 9 6 6 9 9 9 6 c c 9 9 6 6 9 6 
+        6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+        6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        `)
+    autofire_indicator.setPosition(49, 8)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile2 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . 9 9 9 . . . . . 
-        . . . . 9 9 9 9 9 5 9 . . . . . 
-        . . . . . . . . 9 9 9 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, mySprite, 105, 0)
-    projectile2.setFlag(SpriteFlag.AutoDestroy, true)
-    music.pewPew.play()
-    if (powerlevel >= 1) {
-        projectile2.y += -5
+    if (setup_complete == 1) {
         projectile2 = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -46,49 +51,72 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             `, mySprite, 105, 0)
         projectile2.setFlag(SpriteFlag.AutoDestroy, true)
-        projectile2.y += 5
-    }
-    if (powerlevel >= 2) {
-        projectile2 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . 9 9 9 . . . . . 
-            . . . . 9 9 9 9 9 5 9 . . . . . 
-            . . . . . . . . 9 9 9 . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite, 105, 5)
-        projectile2.y += 10
-        projectile2.setFlag(SpriteFlag.AutoDestroy, true)
-        projectile2 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . 9 9 9 . . . . . 
-            . . . . 9 9 9 9 9 5 9 . . . . . 
-            . . . . . . . . 9 9 9 . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite, 105, -5)
-        projectile2.setFlag(SpriteFlag.AutoDestroy, true)
-        projectile2.y += -10
+        music.pewPew.play()
+        if (powerlevel >= 1) {
+            projectile2.y += -5
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . 9 9 9 9 9 5 9 . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, mySprite, 105, 0)
+            projectile2.setFlag(SpriteFlag.AutoDestroy, true)
+            projectile2.y += 5
+        }
+        if (powerlevel >= 2) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . 9 9 9 9 9 5 9 . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, mySprite, 105, 5)
+            projectile2.y += 10
+            projectile2.setFlag(SpriteFlag.AutoDestroy, true)
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . 9 9 9 9 9 5 9 . . . . . 
+                . . . . . . . . 9 9 9 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, mySprite, 105, -5)
+            projectile2.setFlag(SpriteFlag.AutoDestroy, true)
+            projectile2.y += -10
+        }
     }
 })
 function shieldup () {
@@ -116,6 +144,20 @@ function shieldup () {
         `)
     shielded = 1
 }
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    if (autofire == 1) {
+        rapidfirelaser = sprites.createProjectileFromSprite(assets.image`lazer`, mySprite, 105, 0)
+        projectile2.setFlag(SpriteFlag.AutoDestroy, true)
+        projectile_count += 1
+        music.magicWand.play()
+        autofire_indicator.setText("x" + convertToText(1000 - projectile_count * 4))
+        if (projectile_count >= 250) {
+            projectile_count = 0
+            autofire = 0
+            autofire_indicator.destroy()
+        }
+    }
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     otherSprite.destroy(effects.hearts, 200)
     music.baDing.play()
@@ -149,9 +191,10 @@ function createPlayer () {
     animation.runMovementAnimation(
     mySprite,
     animation.animationPresets(animation.easeLeft),
-    500,
+    1000,
     false
     )
+    music.playMelody("G B A B C5 B A B ", 246)
     controller.moveSprite(mySprite)
 }
 function makeShark () {
@@ -196,7 +239,6 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
         music.jumpDown.play()
         info.changeScoreBy(10)
         if (info.score() % 100 == 0) {
-            info.changeLifeBy(1)
             speed += -10
         }
     }
@@ -258,17 +300,24 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         }
     }
 })
+let autofire_sprite: Sprite = null
 let mySprite4: Sprite = null
 let mySprite3: Sprite = null
 let statusbar: StatusBarSprite = null
 let mySprite2: Sprite = null
+let rapidfirelaser: Sprite = null
 let mySprite: Sprite = null
 let projectile2: Sprite = null
+let autofire_indicator: TextSprite = null
+let projectile_count = 0
+let autofire = 0
 let textSprite: TextSprite = null
 let speed = 0
 let shielded = 0
 let powerlevel = 0
 let chests = 0
+let setup_complete = 0
+setup_complete = 0
 effects.starField.startScreenEffect()
 scene.setBackgroundColor(15)
 info.setLife(3)
@@ -276,7 +325,6 @@ chests = 0
 let spawn_rate = 1000
 powerlevel = 0
 shielded = 0
-music.playMelody("G B A B C5 B A B ", 246)
 speed = -20
 textSprite = textsprite.create("x" + convertToText(chests), 15, 5)
 textSprite.setIcon(img`
@@ -299,11 +347,17 @@ textSprite.setIcon(img`
     `)
 textSprite.setPosition(15, 20)
 createPlayer()
+autofire = 0
+projectile_count = 0
+setup_complete = 1
 game.onUpdateInterval(spawn_rate, function () {
     if (game.runtime() > 60000) {
         makeShark()
     }
     if (game.runtime() > 120000) {
+        makeShark()
+    }
+    if (game.runtime() > 180000) {
         makeShark()
     }
     makeShark()
@@ -355,5 +409,29 @@ game.onUpdateInterval(spawn_rate, function () {
         mySprite4.y = randint(20, 100)
         mySprite4.setVelocity(-50, 0)
         mySprite4.setFlag(SpriteFlag.AutoDestroy, false)
+    } else {
+        if (Math.percentChance(1)) {
+            autofire_sprite = sprites.create(img`
+                6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+                6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+                6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+                6 9 6 6 9 9 c c 6 9 9 9 6 6 9 6 
+                6 6 6 9 9 9 9 c 6 6 9 9 9 6 6 6 
+                6 6 9 9 9 9 9 9 6 6 9 9 9 9 6 6 
+                6 9 9 9 6 6 6 6 9 6 9 9 c 9 9 6 
+                6 9 c 6 6 6 9 9 9 6 9 c c c 9 6 
+                6 9 c c c 9 6 9 9 9 6 6 6 c 9 6 
+                6 9 9 c 9 9 6 9 6 6 6 6 9 9 9 6 
+                6 6 9 9 9 9 6 6 9 9 9 9 9 9 6 6 
+                6 6 6 9 9 9 6 6 c 9 9 9 9 6 6 6 
+                6 9 6 6 9 9 9 6 c c 9 9 6 6 9 6 
+                6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+                6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+                6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+                `, SpriteKind.autofire_power)
+            autofire_sprite.x = 160
+            autofire_sprite.setVelocity(-50, 0)
+            autofire_sprite.setFlag(SpriteFlag.AutoDestroy, false)
+        }
     }
 })
